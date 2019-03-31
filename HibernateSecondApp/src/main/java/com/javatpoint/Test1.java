832a51e0d73be6ff89c5347eabbe1b05482a797f
Mahesh <j.mahesh0008@gmail.com>
2019-03-31 23:06:02 +0530

    Added by mahesh -->Test1 file

---
+++ HibernateSecondApp/src/main/java/com/javatpoint/Test1.java
@@ -0,0 +1,17 @@
+package com.javatpoint;
+
+public class Test1 {
+	
+	private String test;
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+	
+	
+
+}
